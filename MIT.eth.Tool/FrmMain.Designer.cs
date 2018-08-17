484764e531021b7522b7a204f0272aa30a407db8
mit <mit.club@yandex.com>
2018-08-17 10:01:11 +0800

    MIT.Eth.Tool

---
+++ MIT.eth.Tool/FrmMain.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MIT.eth.Tool
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnLock = new System.Windows.Forms.Button();
+            this.dtmLock = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(95, 12);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(543, 20);
+            this.txtTo.TabIndex = 9;
+            this.txtTo.Text = "0xb1928f80369c51f95ff0971d888da2a52243816d";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Account:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 90);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(543, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Password:";
+            // 
+            // btnLock
+            // 
+            this.btnLock.Location = new System.Drawing.Point(563, 220);
+            this.btnLock.Name = "btnLock";
+            this.btnLock.Size = new System.Drawing.Size(75, 32);
+            this.btnLock.TabIndex = 12;
+            this.btnLock.Text = "Lock";
+            this.btnLock.UseVisualStyleBackColor = true;
+            this.btnLock.Click += new System.EventHandler(this.btnLock_Click);
+            // 
+            // dtmLock
+            // 
+            this.dtmLock.Location = new System.Drawing.Point(95, 49);
+            this.dtmLock.Name = "dtmLock";
+            this.dtmLock.Size = new System.Drawing.Size(200, 20);
+            this.dtmLock.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Locking period:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(649, 264);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtmLock);
+            this.Controls.Add(this.btnLock);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnLock;
+        private System.Windows.Forms.DateTimePicker dtmLock;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MIT.eth.Tool/FrmMain.cs
@@ -0,0 +1,33 @@
+﻿using MIT.Eth.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIT.eth.Tool
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLock_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dtmLock.Value = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                ClsCommon.WriteLog(ex.ToString(), Consts.M_LogType.M_Error);
+            }
+        }
+    }
+}
